f95eac2803101998ec3b5c284a6d5df50b8f9618
Philip Huang <philiphuang2@gmail.com>
2022-10-22 17:25:38 -0700

    started with Employee.test.js, out to dinner

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,16 @@
+const Employee = require("../lib/Employee");
+
+describe("Employee", () => {
+    describe("Initialization", ()=> {
+        it("should return an error if anything other than 3 arguments are called with the constructor", ()=>{
+            //Create a callback function for 0,1,2,5 arguements
+            //mock the console.log
+            // check against error message
+            const nullEmployee = new Employee();
+        });
+
+        it("should return an object with a 'string' property when called with the new keywords")
+
+    });
+
+});
