8c6ecc1fab89a324cd42edd9e62b75a289508e7f
Philip Huang <philiphuang2@gmail.com>
2022-10-23 02:42:46 -0700

    added initalization tests of employee and added code to make it all work

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,16 +1,52 @@
 const Employee = require("../lib/Employee");
 
 describe("Employee", () => {
+    //Ensures that the 
     describe("Initialization", ()=> {
-        it("should return an error if anything other than 3 arguments are called with the constructor", ()=>{
-            //Create a callback function for 0,1,2,5 arguements
-            //mock the console.log
-            // check against error message
-            const nullEmployee = new Employee();
+        it("should return an object with a 'name' property when called with the correct parameters", ()=>{
+            let badge = 123;
+            const regEmployee = new Employee("Philip", badge, "Philiphuang2@gmail.com");
+            expect("name"in regEmployee).toEqual(true);
         });
 
-        it("should return an object with a 'string' property when called with the new keywords")
+        it("should return an object with a 'id' property when called with the correct parameters", ()=>{
+            let badge = 123;
+            const regEmployee = new Employee("Philip", badge, "Philiphuang2@gmail.com");
+            expect("id"in regEmployee).toEqual(true);
+        });
 
+        it("should return an object with a 'email' property when called with the correct parameters", ()=>{
+            let badge = 123;
+            const regEmployee = new Employee("Philip", badge, "Philiphuang2@gmail.com");
+            expect("email"in regEmployee).toEqual(true);
+        });
+
+        it("should have the 'id' property be a type of Number", ()=>{
+            let badge = 123;
+            const regEmployee = new Employee("Philip", badge, "Philiphuang2@gmail.com");
+            expect(typeof regEmployee.id ).toEqual("number");
+        });
+
+        it("should set 'name' when created", ()=>{
+            let badge = 123;
+            const regEmployee = new Employee("Philip", badge, "Philiphuang2@gmail.com");
+            expect(regEmployee.name).toEqual("Philip");
+        });
+
+        it("should set 'id' when created", ()=>{
+            let badge = 123;
+            const regEmployee = new Employee("Philip", badge, "Philiphuang2@gmail.com");
+            expect(regEmployee.id).toEqual(badge);
+        });
+        it("should set 'email' when created", ()=>{
+            let badge = 123;
+            const regEmployee = new Employee("Philip", badge, "Philiphuang2@gmail.com");
+            expect(regEmployee.email).toEqual("Philiphuang2@gmail.com");
+        });
+        
     });
+    // describe("Member Variables", ()=>{
+    //     it("should return the same number")
+    // })
 
 });
