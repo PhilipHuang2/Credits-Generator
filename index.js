5f7a2e5507317656c76606adcf3a683f5ab9da6e
Philip Huang <philiphuang2@gmail.com>
2022-10-24 23:26:06 -0700

    added first list option

--- index.js
+++ index.js
@@ -27,11 +27,22 @@ Inquirer.prompt(
             type: "number",
             message: "Please enter the team manager's office number",
             name: 'managerOfficeNumber'
+        },
+        // ask user if they want to add more people
+        {
+            type: 'list',
+            message: "What would you like to do",
+            name: "morePeople",
+            choices: ["Add an Engineer", "Add an Intern", "Finish Building the team."]
         }
+        // then if they do, 
+
+        
     ])
     .then(answers => {
         managerInfo = new Manager(answers.managerName, answers.managerID, answers.managerEmail, answers.managerOfficeNumber);
-        
-        console.log(managerInfo);
+
+        console.log("answers:\n:");
+        console.log(answers);
 
     });
